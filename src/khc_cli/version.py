99c3999b6806fbefdd5a25c92b0f98032545c384
Atlas Research <aguennoune@outlook.com>
2025-06-03 11:42:02 +0100

    Bump version to 0.1.1

--- src/khc_cli/version.py
+++ src/khc_cli/version.py
@@ -1,5 +1,5 @@
 """Version information for the khc_cli package."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Krypto Hashers Community"
 __author_email__ = "aguennoune@outlook.com"
